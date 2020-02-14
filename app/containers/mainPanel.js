88c27b0c7e371f6bcaf467c58a90fb0d608a0e0a
khubo(aswin) <aswinrajv@gmail.com>
2020-02-14 11:38:00 +0530

    swap teh order of more and members

--- app/containers/mainPanel.js
+++ app/containers/mainPanel.js
@@ -172,11 +172,10 @@ class MainPanel extends Component {
                 </div>
               </div>
               <div className='channel-meta__other'>
-                <div onClick={this.showCabalSettings.bind(this, this.props.addr)} className='channel-meta__other__more'><img src='static/images/icon-channelother.svg' /></div>
                 <div className='channel-meta__other__members' onClick={toggleMemberList}>
                   <img src='static/images/user-icon.svg' /> {channelMemberCount}
                 </div>
-
+                <div onClick={this.showCabalSettings.bind(this, this.props.addr)} className='channel-meta__other__more'><img src='static/images/icon-channelother.svg' /></div>
                 <div className='button channel-meta__other__share' onClick={self.copyClick.bind(self)}>Share Cabal</div>
 
               </div>
