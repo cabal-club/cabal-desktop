aa2da8c293577976d0adb1b28e3b6d7d700b3072
khubo <aswinrajv@gmail.com>
2020-10-27 13:11:25 +0530

    make app logs configurable

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { config } from 'dotenv'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
@@ -7,13 +8,21 @@ import App from './app'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 
+// load env variables from .env
+config()
+
 // Disable debug console.log messages coming from dependencies
 window.localStorage.removeItem('debug')
 
+const middlewares = [thunk]
+
+if (process.env.ENABLE_APP_LOG) {
+  middlewares.push(logger)
+}
+
 const store = createStore(
   reducer,
-  // compose(applyMiddleware(thunk, logger))
-  compose(applyMiddleware(thunk))
+  compose(applyMiddleware(...middlewares))
 )
 
 render(
