b7416821a8ae03cc1cc70e8b04b62df3228dbd39
khubo <aswinrajv@gmail.com>
2020-11-03 18:37:46 +0530

    cleanup channelPanel

--- app/containers/channelPanel.js
+++ app/containers/channelPanel.js
@@ -3,52 +3,36 @@ import { connect } from 'react-redux'
 
 import {
   hideChannelPanel,
-  leaveChannel,
-  moderationAddAdmin,
-  moderationAddMod,
-  moderationBlock,
-  moderationHide,
-  moderationRemoveAdmin,
-  moderationRemoveMod,
-  moderationUnblock,
-  moderationUnhide
+  leaveChannel
 } from '../actions'
 import MemberList from './memberList'
 
 const mapStateToProps = state => ({
   addr: state.currentCabal,
-  cabal: state.cabals[state.currentCabal]
+  currentChannel: state.cabals[state.currentCabal].channel || ''
 })
 
 const mapDispatchToProps = dispatch => ({
   hideChannelPanel: ({ addr }) => dispatch(hideChannelPanel({ addr })),
-  leaveChannel: ({ addr, channel }) => dispatch(leaveChannel({ addr, channel })),
-  moderationAddAdmin: ({ addr, channel, reason, userKey }) => dispatch(moderationAddAdmin({ addr, channel, reason, userKey })),
-  moderationAddMod: ({ addr, channel, reason, userKey }) => dispatch(moderationAddMod({ addr, channel, reason, userKey })),
-  moderationBlock: ({ addr, channel, reason, userKey }) => dispatch(moderationBlock({ addr, channel, reason, userKey })),
-  moderationHide: ({ addr, channel, reason, userKey }) => dispatch(moderationHide({ addr, channel, reason, userKey })),
-  moderationRemoveAdmin: ({ addr, channel, reason, userKey }) => dispatch(moderationRemoveAdmin({ addr, channel, reason, userKey })),
-  moderationRemoveMod: ({ addr, channel, reason, userKey }) => dispatch(moderationRemoveMod({ addr, channel, reason, userKey })),
-  moderationUnblock: ({ addr, channel, reason, userKey }) => dispatch(moderationUnblock({ addr, channel, reason, userKey })),
-  moderationUnhide: ({ addr, channel, reason, userKey }) => dispatch(moderationUnhide({ addr, channel, reason, userKey }))
+  leaveChannel: ({ addr, channel }) => dispatch(leaveChannel({ addr, channel }))
 })
 
-function ChannelPanel (props) {
+function ChannelPanel ({ currentChannel, addr, hideChannelPanel, leaveChannel }) {
   function onClickLeaveChannel () {
-    props.leaveChannel({
-      addr: props.cabal.addr,
-      channel: props.cabal.channel
+    leaveChannel({
+      addr,
+      channel: currentChannel
     })
   }
 
-  const canLeave = props.cabal.channel !== '!status'
-  const hasMembers = props.cabal.channel !== '!status'
+  const canLeave = currentChannel !== '!status' && !!currentChannel
+  const hasMembers = currentChannel !== '!status'
 
   return (
     <div className='panel ChannelPanel'>
       <div className='panel__header'>
         Channel Details
-        <span onClick={() => props.hideChannelPanel({ addr: props.addr })} className='close'><img src='static/images/icon-composermeta.svg' /></span>
+        <span onClick={() => hideChannelPanel({ addr })} className='close'><img src='static/images/icon-composermeta.svg' /></span>
       </div>
       {canLeave &&
         <div className='panel__content'>
@@ -64,7 +48,7 @@ function ChannelPanel (props) {
             Channel Members
           </div>
           <div className='panel__content'>
-            <MemberList addr={props.addr} />
+            <MemberList addr={addr} />
           </div>
         </>}
     </div>
